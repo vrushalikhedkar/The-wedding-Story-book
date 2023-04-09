f798320cd10d76b97e8b47bc9551004605d755db
yogitashete2412 <yogitashete24@gmail.com>
2023-04-09 17:39:46 +0530

    javascript added for first row of cards

--- JS/MakeUp.js
+++ JS/MakeUp.js
@@ -6,3 +6,21 @@ function imgOut() {
     const imgElement = document.getElementById("img");
     imgElement.src = "../Images/MakeUpImg/makeup-img-1.jpg";
 }
+
+function imgIn1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "../Images/MakeUpImg/make-up-9.jpg";
+}
+function imgOut1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "../Images/MakeUpImg/make-up-1.jpg";
+}
+
+function imgIn2() {
+    const imgElement = document.getElementById("img2");
+    imgElement.src = "../Images/MakeUpImg/make-up-21.jpeg";
+}
+function imgOut2() {
+    const imgElement = document.getElementById("img2");
+    imgElement.src = "../Images/MakeUpImg/make-up-3.jpg";
+}
