7b2df10fcce5323630991452c7a919b6cc53e751
yogitashete2412 <yogitashete24@gmail.com>
2023-04-09 17:41:13 +0530

    js added for bridal makeup cards

--- JS/MakeUp.js
+++ JS/MakeUp.js
@@ -24,3 +24,92 @@ function imgOut2() {
     const imgElement = document.getElementById("img2");
     imgElement.src = "../Images/MakeUpImg/make-up-3.jpg";
 }
+
+function imgIn3() {
+    const imgElement = document.getElementById("img3");
+    imgElement.src = "../Images/MakeUpImg/sangeet-3.jpeg";
+}
+function imgOut3() {
+    const imgElement = document.getElementById("img3");
+    imgElement.src = "../Images/MakeUpImg/make-up-4.jpg";
+}
+
+function imgIn4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "../Images/MakeUpImg/sangeet-2.jpeg";
+}
+function imgOut4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "../Images/MakeUpImg/make-up-18.jpg";
+}
+
+function imgIn5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "../Images/MakeUpImg/engagement-12.jpg";
+}
+function imgOut5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "../Images/MakeUpImg/make-up-15.jpg";
+}
+//  ....
+
+function imgIn6() {
+    const imgElement = document.getElementById("img6");
+    imgElement.src = "../Images/MakeUpImg/make-up-2.jpg";
+}
+function imgOut6() {
+    const imgElement = document.getElementById("img6");
+    imgElement.src = "../Images/MakeUpImg/make-up-5.jpg";
+}
+
+
+function imgIn7() {
+    const imgElement = document.getElementById("img7");
+    imgElement.src = "../Images/MakeUpImg/reception-4.jpg";
+}
+function imgOut7() {
+    const imgElement = document.getElementById("img7");
+    imgElement.src = "../Images/MakeUpImg/make-up-9.jpg";
+}
+
+
+function imgIn8() {
+    const imgElement = document.getElementById("img8");
+    imgElement.src = "../Images/MakeUpImg/sangeet-8.jpeg";
+}
+function imgOut8() {
+    const imgElement = document.getElementById("img8");
+    imgElement.src = "../Images/MakeUpImg/make-up-16.jpg";
+}
+
+
+function imgIn9() {
+    const imgElement = document.getElementById("img9");
+    imgElement.src = "../Images/MakeUpImg/reception-17.jpg";
+}
+function imgOut9() {
+    const imgElement = document.getElementById("img9");
+    imgElement.src = "../Images/MakeUpImg/make-up-23.jpg";
+}
+
+
+function imgIn10() {
+    const imgElement = document.getElementById("img10");
+    imgElement.src = "../Images/MakeUpImg/make-up-5.jpg";
+}
+function imgOut10() {
+    const imgElement = document.getElementById("img10");
+    imgElement.src = "../Images/MakeUpImg/make-up-22.jpg";
+}
+
+
+function imgIn11() {
+    const imgElement = document.getElementById("img11");
+    imgElement.src = "../Images/MakeUpImg/make-up-4.jpg";
+}
+function imgOut11() {
+    const imgElement = document.getElementById("img11");
+    imgElement.src = "../Images/MakeUpImg/make-up-21.jpeg";
+}
+
+
