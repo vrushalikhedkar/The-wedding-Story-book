347c5876c7034e7762ae6eb1722a3c1d91729099
yogitashete2412 <yogitashete24@gmail.com>
2023-04-11 21:37:48 +0530

    updated

--- JS/photography.js
+++ JS/photography.js
@@ -24,3 +24,119 @@ function imgOut2() {
     const imgElement = document.getElementById("img2");
     imgElement.src = "../Images/Photo-galleryImg/Img-1.jpg";
 }
+
+function imgIn3() {
+    const imgElement = document.getElementById("img3");
+    imgElement.src = "../Images/Photo-galleryImg/wedding-13.jpg";
+}
+function imgOut3() {
+    const imgElement = document.getElementById("img3");
+    imgElement.src = "../Images/Photo-galleryImg/Img-3.jpg";
+}
+
+function imgIn4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "../Images/Photo-galleryImg/wedding-11.jpg";
+}
+function imgOut4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "../Images/Photo-galleryImg/Img-25.jpg";
+}
+
+function imgIn5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "../Images/Photo-galleryImg/img-19.jpg";
+}
+function imgOut5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "../Images/Photo-galleryImg/Img-24.jpg";
+}
+
+
+function imgIn6() {
+    const imgElement = document.getElementById("img6");
+    imgElement.src = "../Images/Photo-galleryImg/wedding-2.webp";
+}
+function imgOut6() {
+    const imgElement = document.getElementById("img6");
+    imgElement.src = "../Images/Photo-galleryImg/wedding-1.webp";
+}
+
+
+// function imgIn7() {
+//     const imgElement = document.getElementById("img7");
+//     imgElement.src = "../Images/Photo-galleryImg/wedding-1.webp";
+// }
+// function imgOut7() {
+//     const imgElement = document.getElementById("img7");
+//     imgElement.src = "../Images/Photo-galleryImg/wedding-4.webp";
+// }
+
+
+// function imgIn8() {
+//     const imgElement = document.getElementById("img8");
+//     imgElement.src = "../Images/MakeUpImg/sangeet-8.jpeg";
+// }
+// function imgOut8() {
+//     const imgElement = document.getElementById("img8");
+//     imgElement.src = "../Images/MakeUpImg/make-up-16.jpg";
+// }
+
+
+// function imgIn9() {
+//     const imgElement = document.getElementById("img9");
+//     imgElement.src = "../Images/MakeUpImg/reception-17.jpg";
+// }
+// function imgOut9() {
+//     const imgElement = document.getElementById("img9");
+//     imgElement.src = "../Images/MakeUpImg/make-up-23.jpg";
+// }
+
+
+// function imgIn10() {
+//     const imgElement = document.getElementById("img10");
+//     imgElement.src = "../Images/MakeUpImg/make-up-5.jpg";
+// }
+// function imgOut10() {
+//     const imgElement = document.getElementById("img10");
+//     imgElement.src = "../Images/MakeUpImg/make-up-22.jpg";
+// }
+
+
+// function imgIn11() {
+//     const imgElement = document.getElementById("img11");
+//     imgElement.src = "../Images/MakeUpImg/make-up-4.jpg";
+// }
+// function imgOut11() {
+//     const imgElement = document.getElementById("img11");
+//     imgElement.src = "../Images/MakeUpImg/make-up-21.jpeg";
+// }
+
+function imgIn12() {
+    const imgElement = document.getElementById("img12");
+    imgElement.src = "../Images/Photo-galleryImg/prewedding-14.jpg";
+}
+function imgOut12() {
+    const imgElement = document.getElementById("img12");
+    imgElement.src = "../Images/MakeUpImg/engagement-4.jpg";
+}
+
+function imgIn13() {
+    const imgElement = document.getElementById("img13");
+    imgElement.src = "../Images/Photo-galleryImg/unique-10.jpg";
+}
+function imgOut13() {
+    const imgElement = document.getElementById("img13");
+    imgElement.src = "../Images/Photo-galleryImg/prewedding-10.jpg";
+}
+
+function imgIn14() {
+    const imgElement = document.getElementById("img14");
+    imgElement.src = "../Images/Photo-galleryImg/beach-10.jpg";
+}
+function imgOut14() {
+    const imgElement = document.getElementById("img14");
+    imgElement.src = "../Images/MakeUpImg/engagement-3.jpg";
+}
+
+
