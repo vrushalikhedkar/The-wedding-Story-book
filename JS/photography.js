558e8db692497a1d1d1d24209adb97a603858f30
yogitashete2412 <yogitashete24@gmail.com>
2023-04-11 21:36:08 +0530

    js added to first row

---
+++ JS/photography.js
@@ -0,0 +1,26 @@
+function imgIn() {
+    const imgElement = document.getElementById("img");
+    imgElement.src = "../Images/Photo-galleryImg/img-23.jpg";
+ }
+ function imgOut() {
+    const imgElement = document.getElementById("img");
+     imgElement.src = "../Images/Photo-galleryImg/Img-19.jpg";
+}
+
+function imgIn1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "../Images/Photo-galleryImg/Img-3.jpg";
+}
+function imgOut1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "../Images/Photo-galleryImg/Img-20.jpg";
+}
+
+function imgIn2() {
+    const imgElement = document.getElementById("img2");
+    imgElement.src = "../Images/Photo-galleryImg/Img-25.jpg";
+}
+function imgOut2() {
+    const imgElement = document.getElementById("img2");
+    imgElement.src = "../Images/Photo-galleryImg/Img-1.jpg";
+}
