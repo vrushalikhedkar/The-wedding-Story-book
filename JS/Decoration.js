f0e650090d2907de5787c0e1bfca70e70ea1d56d
Vrushali Khedkar <vrushalikhedkar0704@gmail.com>
2023-04-12 16:44:55 +0530

    completed

---
+++ JS/Decoration.js
@@ -0,0 +1,9 @@
+function Light() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "body";
+}
+
+function Dark() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "Dark";
+}
