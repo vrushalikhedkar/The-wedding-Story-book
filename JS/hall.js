12020de156c06aabc5903a8ea764073c292c252d
Pushpakparkhe <pushpakparkhe2@gmail.com>
2023-04-12 14:26:05 +0530

    navbar

--- JS/hall.js
+++ JS/hall.js
@@ -6,4 +6,13 @@ function setOldImage(){
     document.getElementById("img").src ="../Images/hall/review1 (1).jpg";
 
 }
+function Light() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "body";
+}
+
+function Dark() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "Dark";
+}
 
--- JS/vendor.js
+++ JS/vendor.js
@@ -9,4 +9,13 @@ function setOldImage(){
     document.getElementById("img").src ="../Images/vendor/js2.webp";
 
 }
+function Light() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "body";
+}
+
+function Dark() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "Dark";
+}
 
