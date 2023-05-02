5b282f62dde7c86541b39f23aae66c19f64d3e88
neha pawar <itsnehu13@gmail.com>
2023-05-02 14:27:15 +0530

    image JS added

---
+++ JS/mehendi.js
@@ -0,0 +1,17 @@
+function imgIn() {
+    const imgElement = document.getElementById("img");
+    imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/243192335_916679272534053_6329677032693561214_n.jpg";
+ }
+ function imgOut() {
+    const imgElement = document.getElementById("img");
+     imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/243371409_392471005783294_3200694204528961826_n.jpg";
+}
+
+function imgIn1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/rose1.jpg";
+}
+function imgOut1() {
+    const imgElement = document.getElementById("img1");
+    imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/rose2.jpg";
+}
