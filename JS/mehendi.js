9a29318c67a70dcacf908374bbd2fcc4294fb21a
neha pawar <itsnehu13@gmail.com>
2023-05-02 14:27:50 +0530

    image JS added

--- JS/mehendi.js
+++ JS/mehendi.js
@@ -33,3 +33,21 @@ function imgOut3() {
     const imgElement = document.getElementById("img3");
     imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/1-7.jpg.jpg";
 }
+
+function imgIn4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "../Images/Mehendi/personalize js.jpg";
+}
+function imgOut4() {
+    const imgElement = document.getElementById("img4");
+    imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2017/09/1487064299_Mehndi_9.jpg";
+}
+
+function imgIn5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "../Images/Mehendi/get js.jpg";
+}
+function imgOut5() {
+    const imgElement = document.getElementById("img5");
+    imgElement.src = "https://image.wedmegood.com/resized-nw/1300X/wp-content/uploads/2019/03/1528358098_new_mehe.jpg";
+}
